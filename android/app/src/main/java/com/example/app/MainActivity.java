067e6fe56fa2a6a35f90116e373a3da6cd67d65a
YOGESHWARAN-FAF <yogifaf03@gmail.com>
2025-12-12 18:36:11 +0530

    Capacitor Android fixed build - SDK 35, minSdk 23, Kotlin fix-chekkkk

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,25 @@
 package com.example.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            WebSettings settings = webView.getSettings();
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            settings.setJavaScriptEnabled(true);
+            settings.setDomStorageEnabled(true);
+        }
+    }
+}
